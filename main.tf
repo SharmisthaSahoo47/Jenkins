5df1469d1b743c47893e51177c5ccb31c855f0dc
SharmisthaSahoo47 <sharmistha500@gmail.com>
2025-05-07 20:32:29 +0530

    Create main.tf
    main.tf added

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-002f6e91abff6eb96"
+    instance_type = "t2.nano"
+    tags = {
+      Name = "devserver"
+    }
+}
